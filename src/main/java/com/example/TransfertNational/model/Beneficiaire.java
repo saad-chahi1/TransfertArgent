3c5d0f5c4a3804f2128d04d62726fc5758c0fb55
saad-chahi1 <saadcode123@gmail.com>
2022-01-10 10:52:06 +0100

    ImplementSomeClientMethod

--- src/main/java/com/example/TransfertNational/model/Beneficiaire.java
+++ src/main/java/com/example/TransfertNational/model/Beneficiaire.java
@@ -5,13 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Data
+import javax.persistence.*;
+
+@Data @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class Beneficiaire {
+    @Id
     private int Id;
     private String Nom;
     private String Prenom;
     private String GSM;
+
 }
